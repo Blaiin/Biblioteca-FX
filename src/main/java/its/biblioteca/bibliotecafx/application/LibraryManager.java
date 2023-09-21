0a17d3670e4889aa0ab1334f23de773f3e2ee758
Blaiin <96704158+Blaiin@users.noreply.github.com>
2023-09-21 12:36:08 +0200

    css styling

--- src/main/java/its/biblioteca/bibliotecafx/application/LibraryManager.java
+++ src/main/java/its/biblioteca/bibliotecafx/application/LibraryManager.java
@@ -4,8 +4,8 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-
 import java.net.URL;
+import java.util.Objects;
 
 public class LibraryManager extends Application {
 
@@ -15,19 +15,12 @@ public class LibraryManager extends Application {
             OverviewController overView = new OverviewController();
             URL resourceURL = getClass().getResource("Overview.fxml");
                 FXMLLoader fxmlLoader = new FXMLLoader(resourceURL);
-                Scene scene = new Scene(fxmlLoader.load());
-                stage.getIcons().add(overView.getIcon());
-                stage.setTitle("Library Overview");
-                stage.setScene(scene);
-                stage.show();
-            //System.out.println(resourceURL);
-            /* else {
-                System.err.println("Resource not found: " + resourceURL);
-                throw new NullPointerException();
-                // Handle the case where the resource is not found.
-            }*/
-            //scene.getStylesheets().add(Objects.requireNonNull(getClass().getResource("/resources/application.css")).toExternalForm());
-
+            Scene scene = new Scene(fxmlLoader.load());
+            scene.getStylesheets().add(Objects.requireNonNull(getClass().getResource("Overview.css")).toExternalForm());
+            stage.getIcons().add(overView.getIcon());
+            stage.setTitle("Library Overview");
+            stage.setScene(scene);
+            stage.show();
         } catch(Exception e) {
             System.out.println("Errore: ");
             e.printStackTrace();
--- src/main/java/its/biblioteca/bibliotecafx/application/OverviewController.java
+++ src/main/java/its/biblioteca/bibliotecafx/application/OverviewController.java
@@ -6,14 +6,19 @@ import javafx.scene.control.Button;
 import javafx.scene.image.Image;
 import java.io.File;
 import javafx.fxml.FXML;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class OverviewController implements Initializable {
-    private static char sep = File.separator.charAt(0);
+    private static final char SEP = File.separator.charAt(0);
     private static final String currentDirectory = System.getProperty("user.dir");
-    private static final String dynamicOutputPath = currentDirectory + sep + "Archive" + sep;
+    private static final String dynamicOutputPath = currentDirectory + SEP + "Archive" + SEP;
     private final Image icon = new Image(String.valueOf(getClass().getResource("Library1.png")));
+    @FXML
+    AnchorPane anchorPane;
     @FXML
     Button addUserButton;
     @FXML
@@ -22,9 +27,13 @@ public class OverviewController implements Initializable {
     Button addRentButton;
     @FXML
     Button showHistoryButton;
+    private final Image booksStack = new Image(String.valueOf(getClass().getResource("BooksStack.png")));
+    @FXML
+    ImageView overviewImage;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+        overviewImage.setImage(booksStack);
         Archive archive = new Archive();
         archive.fileManager.manageFileJSON(archive.fileManager.getUsersJson());
         archive.fileManager.manageFileJSON(archive.fileManager.getBooksJson());
